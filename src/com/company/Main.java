7b390a6bcd879d321a25de816deb36764d7fb074
VengerovskayaAlena <31alena_vengerovskaya@mail.ru>
2023-11-23 11:26:40 +0300

    attempt5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,29 +2,20 @@ package com.company;
 
 public class Main {
 
-    public static Book creatBook(){
-        Book book = new Book(title, author, age);
-        book.title = title;
-        book.author = author;
-        book.age = age;
-        return book;
-    }
-
-    public static Author creatAuthor(){
-        Author author = new Author(name, surname);
-        author.name = name;
-        author.surname = surname;
-        return author;
-    }
 
     public static void main(String[] args) {
-        Author author1 = creatAuthor("Maxim", "Gorky");
 
-        Author author2 = creatAuthor("Lucy", "Montgomery");
+        Author author1 = new Author("Maxim", "Gorky");
+        Book book1 = new Book("At the bottom", author1, 1902);
+        System.out.println("Книга 1: " + title + author1 + age);
 
-        Book book1 = creatBook("At the bottom", author1, 1902);
 
-        Book book2 = creatBook("Ann of Green Gables", author2, 1908);
+        Author author2 = new Author("Lucy", "Montgomery");
+        Book book2 = new Book("Ann of Green Gables", author2, 1908);
+        System.out.println("Книга 2: " + title + author2 + age);
+
 
         book1.setAge(2000);
+
+
 }}
